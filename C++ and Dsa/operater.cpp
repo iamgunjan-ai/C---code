b980582f1a1244d459c78e486382f2f6abfb6af9
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-23 13:50:58 +0530

    Add files via upload

---
+++ C++ and Dsa/operater.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+    {
+        int a=5;
+        int b=10;
+        cout<<"sum:";
+        cout<< a+b<<endl;
+        cout<<"sub:";
+        cout<< a-b<<endl;
+        cout<<"mul:";
+        cout<< a*b<<endl;
+        cout<<"div:";
+        cout<< a/b<<endl;
+
+
+
+}
